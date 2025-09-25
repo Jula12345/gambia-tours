96a35be4a8aa0f8d0f51aae25e8ecf378864e71a
mino <mino@minos-air.home>
2025-09-25 20:27:57 +0200

    Fix reservation form, add logo, burger menu, and more text

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-// Add JS for gallery modal, interactivity, etc.
+// Burger menu functionality for mobile navigation
+function toggleMenu() {
+	const links = document.getElementById('navbarLinks');
+	links.classList.toggle('open');
+}
